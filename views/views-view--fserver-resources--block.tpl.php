f5724f1168e21806ec059ea921f7bbeec8e9a15d
Florian Latzel <florian.latzel@is-loesungen.de>
2012-06-09 12:15:28 +0200

    Removed unneeded 'empty' statement from views template.

--- views/views-view--fserver-resources--block.tpl.php
+++ views/views-view--fserver-resources--block.tpl.php
@@ -39,16 +39,9 @@
       <?php print strip_tags($header); ?>
     </div>
   <?php endif; ?>
-
   <?php if ($rows): ?>
     <div class="view-content">
       <?php print $rows; ?>
     </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-      <?php print 'leer'; ?>
-    </div>
   <?php endif; ?>
-
 </div> <?php /* class view */ ?>
